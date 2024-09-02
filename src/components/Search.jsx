90a1e5a3d61298bbe53219d3dd96ea9cabf3c026
himudit <gargmudit662@gmail.com>
2024-09-02 15:05:05 +0530

    adding profile

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import RecipeApp from './RecipeApp';
 import CookingAnimation from './Preloader/Preloader';
+import { useNavigate } from 'react-router-dom';
+import images from '../assets/img1.jpg';
 
 function ParentComponent() {
   const [dish, setDish] = useState('');
   const [submittedDish, setSubmittedDish] = useState('');
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
   const handleInputChange = (e) => {
     setDish(e.target.value);
@@ -14,6 +17,10 @@ function ParentComponent() {
   const handleSubmit = (e) => {
     e.preventDefault();
     setSubmittedDish(dish);
+  };
+
+  const handleProfileClick = () => {
+    navigate('/profile');
   };
 
   useEffect(() => {
@@ -32,6 +39,17 @@ function ParentComponent() {
       <div
         className="flex flex-col justify-center items-center mt-4 md:mt-8 lg:mt-12 ml-12 md:ml-20 lg:ml-32  bg-no-repeat relative"
       >
+        {/* Profile Picture */}
+        <div className="absolute top-1 right-4 md:top-3 md:right-6 lg:top-4 lg:right-8">
+          <img
+            src={images} // Replace with the actual path to the profile picture
+            alt="Profile"
+            className="h-14 w-14 md:h-12 md:w-12 lg:h-14 lg:w-14 rounded-full cursor-pointer"
+            onClick={handleProfileClick}
+          />
+        </div>
+
+
         {/* Background text */}
         <div className="text-5xl md:text-7xl lg:text-8xl font-bold text-gray-300 opacity-30" style={{ top: '50%', transform: 'translateY(-50%)' }}>
           Healthy Food
